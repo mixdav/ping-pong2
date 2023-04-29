d0c652c039175334a3eb6da216b66ee51da2d53b
mixdav <131501472+mixdav@users.noreply.github.com>
2023-04-29 11:56:36 +1100

    pinpong v0.2
    Добавлена обработка выигрыша

--- ping-pong.py
+++ ping-pong.py
@@ -15,37 +15,43 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def move_1(self):
         key_push = key.get_pressed()
-        if key_push[K_UP] and self.rect.y<10:
+        if key_push[K_UP] and self.rect.y>10:
+            self.rect.y -= self.speed
+        if key_push[K_DOWN] and self.rect.y<380:
             self.rect.y += self.speed
-        if key_push[K_DOWN] and self.rect.y>5:
-            self.rect.x -= self.speed
-    def move_1(self):
+    def move_2(self):
         key_push = key.get_pressed()
-        if key_push[k_w] and self.rect.y<10:
-            self.rect.x += self.speed
-        if key_push[k_s] and self.rect.y>5:
-            self.rect.x -= self.speed
-    def fire(self):
-        bullet = Bullet('bullet.png',self.rect.centerx, self.rect.top, 10, 10, 15)
-        bullets.add(bullet)
+        if key_push[K_w] and self.rect.y>10:
+            self.rect.y -= self.speed
+        if key_push[K_s] and self.rect.y<380:
+            self.rect.y += self.speed
 
 
-
-glav_window = display.set_mode((700,500))
-display.set_caption('pygame window')
-background = transform.scale(image.load('fon.PNG'), (700,500))
+fon = (200,250,240)
+glav_window = display.set_mode((800,600))
+glav_window.fill(fon)
 
 
 
 game = True
 clock = time.Clock()
-FPS = 60
+FPS = 100
 finish = False
+speed_x = 3
+speed_y = 3
 
+score_1 = 0
+score_2 = 0
 
+font.init()
+font_1 = font.SysFont('Arial',50)
+font_2 = font.SysFont('Arial',40)
+pobeda_1 = font_2.render('Победил первый игрок!', True, (255,215,3))
+pobeda_2 = font_2.render('Победил второй игрок!', True, (255,215,3))
 
-player_1 = Player('racetka.png', 100, 420, 15, 60, 65)
-player_2 = Player('racetka.png', 650, 420, 15, 60, 65)
+ball = GameSprite('appel.png',250,200,5,90,90)
+player_1 = Player('111.png',650,350,15,150,200)
+player_2 = Player('111.png',10,350,15,150,200)
 
 
 
@@ -55,20 +61,32 @@ while game:
             game = False
 
     if finish != True:
-        glav_window.blit(background, (0,0))
+        glav_window.fill(fon)
 
 
+        player_1.move_1()
+        player_2.move_2()
 
-        player_1.move()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(player_1,ball) or sprite.collide_rect(player_2,ball):
+            speed_x *= -1
+            speed_y *= 1
+        
+        if ball.rect.y>400 or ball.rect.y<0:
+            speed_y *= -1
+
+        if ball.rect.x < 10:
+            glav_window.blit(pobeda_1,(30,20))
+        if ball.rect.x > 800:
+            glav_window.blit(pobeda_2,(30,20))          
+        
+        
+
+        ball.reset()
         player_1.reset()
+        player_2.reset()
 
-        player_2.move()
-        player_2.reset()       
-
-
-
-
-
-
-        display.update()
-        clock.tick(FPS)
+    display.update()
+    clock.tick(FPS)
